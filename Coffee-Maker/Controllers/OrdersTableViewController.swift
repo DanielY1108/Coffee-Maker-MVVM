653d8a509174c176c9e0c766ce71b041fcd364b4
DanielY1108 <scarlet040@gmail.com>
2023-02-08 00:51:35 +0900

    url 가져오기 성공

--- Coffee-Maker/Controllers/OrdersTableViewController.swift
+++ Coffee-Maker/Controllers/OrdersTableViewController.swift
@@ -12,6 +12,28 @@ class OrdersTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        Task.detached {
+            await self.populateOrders()
+        }
+        
     }
     
+    private func populateOrders() async {
+        guard let coffeeOrdersURL = URL(string: "https://warp-wiry-rugby.glitch.me/orders") else {
+            fatalError("URL was incorrect")
+        }
+        
+        let resource = Resource<[Order]>(url: coffeeOrdersURL)
+        
+        let result = await Webservice().load(resource: resource)
+        
+        switch result {
+        case .success(let orders):
+            print(orders)
+        case .failure(let error):
+            print(error)
+        }
+        
+        
+    }
 }
